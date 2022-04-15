0ce4aefcc65bd21ff3da191c94eec5c0159442db
Alex Ntavelos <a.ntavelos@gmail.com>
2022-04-15 14:06:36 +0200

    handle case were ICAO_CODES env variable is not set

--- met_update/config/__init__.py
+++ met_update/config/__init__.py
@@ -1,5 +1,5 @@
 from os import getenv
 
 update_rate = int(getenv('UPDATE_RATE', 30))
-icao_codes = getenv('ICAO_CODES').split(',') or ['EHAM', 'LFPO', 'LFBO']
+icao_codes = getenv('ICAO_CODES', 'EHAM,LFPO,LFBO').split(',')
 avwx_token = getenv('AVWX_TOKEN')
