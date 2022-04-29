fea5f1865ea22989ce7495503a7ed29ad9c001f6
Alex Ntavelos <a.ntavelos@gmail.com>
2022-04-29 11:53:35 +0200

    update default airports

--- met_update/config/__init__.py
+++ met_update/config/__init__.py
@@ -1,5 +1,5 @@
 from os import getenv
 
 update_rate = int(getenv('UPDATE_RATE', 30))
-icao_codes = getenv('ICAO_CODES', 'EHAM,LFPO,LFBO').split(',')
+icao_codes = getenv('ICAO_CODES', 'EHAM,LEMD,LFPO,LOWW').split(',')
 avwx_token = getenv('AVWX_TOKEN')
